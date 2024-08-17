f674611aec2a68d961df3a521c0c4cda97d0f8c9
Ramya Sharma <144830248+Ramya-GLA@users.noreply.github.com>
2024-08-17 20:55:29 +0530

    Add files via upload

---
+++ even odd.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+    class ex{
+      public static void main (String arg[]){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number");
+        int a =sc.nextInt();
+        if (a%2==0){
+          System.out.println("Even");}
+          else{
+            System.out.println(" Odd");
+          }
+      }
+    }
+    
+    
+
