fd1b3f90b8aabbdf5174b0d7b5fcca32e96f0fe7
Ramya Sharma <144830248+Ramya-GLA@users.noreply.github.com>
2024-08-18 23:49:12 +0530

    Add files via upload

---
+++ lcm.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class lcm {
+    public static void main (String[] arg){
+        Scanner sc = new Scanner(System.in);
+        int A=sc.nextInt();
+        int B =sc.nextInt();
+        int LCM=(A>B)? A:B;
+        while(LCM % A != 0 || LCM % B != 0) {
+            LCM++;
+        }
+
+      
+        System.out.println(LCM);
+
+     
+    }
+}
