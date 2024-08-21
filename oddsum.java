01733b736c076584b13afc5eea178e75d03802ac
Ramya Sharma <144830248+Ramya-GLA@users.noreply.github.com>
2024-08-21 21:44:41 +0530

    Add files via upload

---
+++ oddsum.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class oddsum {
+   public static void main (String[] args){
+    Scanner sc= new Scanner(System.in);
+    int A = sc.nextInt();
+    int sum=0;
+    for(int i =1;i<=A;i++){
+        if (i% 2 !=0){
+            sum+=i;
+
+        }
+    }
+    System.out.println(sum);
+   }
+}
