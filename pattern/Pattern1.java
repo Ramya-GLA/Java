bc3085a5cee44bbd1ed1164f69cf20e2b93e53da
Ramya Sharma <144830248+Ramya-Sharma@users.noreply.github.com>
2024-08-22 10:53:28 +0530

    Create Pattern1.java

---
+++ pattern/Pattern1.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class pattern1 {
+    public static void main(String[] arg){
+     Scanner sc= new Scanner(System.in);
+     int a= sc.nextInt();
+     for( int i=0;i<=a;i++){
+        for(int j=1;j<=i;j++){
+            System.out.print("*");
+        }
+       System.out.println();
+     }
+    }
+    
+}
