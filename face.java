d7cc99342669b1b5d5049b9d289a3afd11209d0b
Ramya Sharma <144830248+Ramya-Sharma@users.noreply.github.com>
2024-08-23 11:23:33 +0530

    Add files via upload

---
+++ face.java
@@ -0,0 +1,13 @@
+public class face {
+        public static void main(String[] args)
+        {
+            // Display a pattern to create an ASCII art representation of a simple face
+            System.out.println(" +\"\"\"\"\"+ ");
+            System.out.println("[| o o |]");
+            System.out.println(" |  ^  | ");
+            System.out.println(" | '-' | ");
+            System.out.println(" +-----+ ");
+        }
+    }
+    
+
